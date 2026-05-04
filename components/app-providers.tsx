ef93ea996a2be8f3bea13923075d16a6e3190eb6
Marcos Antonio <heymmark@gmail.com>
2026-05-03 22:36:36 -0300

    Add TooltipProvider to layout and update navigation with new components

---
+++ components/app-providers.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+import * as React from "react"
+import { TooltipProvider } from "@/components/ui/tooltip"
+
+export function AppProviders({ children }: { children: React.ReactNode }) {
+  return <TooltipProvider>{children}</TooltipProvider>
+}
